89edd5642d1c3a625b712f4b4343879242411ae0
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2021-09-29 12:01:22 +0200

    files added

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require("chalk");
+
+
+console.log(chalk.cyan("charlotte"));
+console.log(chalk.blue("Mounir"));
+console.log(chalk.green("Etienne"));
+console.log(chalk.yellow("Philippe"));
